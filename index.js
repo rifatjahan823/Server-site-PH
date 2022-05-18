b5d71cb7855def3cbfcdd9af8dc14133cf6818ac
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-19 00:07:19 +0600

    add addtask api for show user task

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 //use middleware
 app.use(cors());
 app.use(express.json());
 
 
-// Secret-Task-React-PH
-//SnImRZ8QXEongUab
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Secret-Task-React-PH:<password>@cluster0.61x5s.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.SECRET_TASK_USER}:${process.env.SECRET_TASK_PASS}@cluster0.61x5s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+    try {
+      await client.connect();
+      const taskCollection = client.db("Secret-Task-React-PH").collection("task");
+
+ //post and add new inventory
+ app.post("/addtask",async(req,res)=>{
+  const task = req.body;
+  const getUserTask = await taskCollection.insertOne(task);
+  res.send(getUserTask )
+})
+//show user task 
+app.get("/addtask",async(req,res)=>{
+    const query = {};
+    const cursor = taskCollection.find(query);
+    const tasks = await cursor.toArray();
+    res.send(tasks );
+})
+
+    } 
+    finally {
+    
+    }
+  }
+  run().catch(console.dir);
 
 
 
